60831fdb352ad9c62b993647721432ce24b71853
Didarulislam-Ai <diderjustin@gmail.com>
2021-07-15 13:49:19 -0400

    working react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,107 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+function Welcome(props) {
+  if (props.name.length > 4) {
+    return <h1>Hello, {props.name}</h1>
+  }
+  else {
+    return <p> too short</p>
+  }
 }
 
-export default App;
+
+export default function App() {
+  return (
+    <Router>
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/users">Users</Link>
+          </li>
+        </ul>
+      </nav>
+
+      {/* A <Switch> looks through its children <Route>s and
+          renders the first one that matches the current URL. */}
+      <Switch>
+        <Route path="/about">
+            {/* <About /> */}
+            <div className="App">
+            <header className="App-header">
+              <Welcome name="Didarul"/>
+              <img src={logo} className="App-logo" alt="logo" />
+              <p>
+                About <code>src/App.js</code> and save to reload.
+              </p>
+              <a
+                className="App-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Learn React
+              </a>
+            </header>
+            </div>
+        </Route>
+          <Route path="/users">
+                <div className="App">
+            <header className="App-header">
+              <Welcome name="Didarul"/>
+              <img src={logo} className="App-logo" alt="logo" />
+              <p>
+                Edit <code>src/App.js</code> and save to reload.
+              </p>
+              <a
+                className="App-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Learn React
+              </a>
+            </header>
+          </div>
+          {/* <Users /> */}
+        </Route>
+        <Route path="/">
+            {/* <Home /> */}
+            <div className="App">
+            <header className="App-header">
+              <Welcome name="Didarul"/>
+              <img src={logo} className="App-logo" alt="logo" />
+              <p>
+                Home <code>s rc/App.js</code> and save to reload.
+              </p>
+              <a
+                className="App-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Learn React
+              </a>
+            </header>
+          </div>
+        </Route>
+      </Switch>
+    </div>
+  </Router>
+);
+}
