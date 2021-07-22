889d96acc92c67209e63108386b760978743629f
Didarulislam-Ai <diderjustin@gmail.com>
2021-07-22 12:32:20 -0400

    working react day2

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
 
-import React from "react";
+//Import react-routerdom
+import React from 'react';
 import {
   BrowserRouter as Router,
   Switch,
   Route,
   Link
-} from "react-router-dom";
+} from 'react-router-dom';
 
-function Welcome(props) {
-  if (props.name.length > 4) {
-    return <h1>Hello, {props.name}</h1>
-  }
-  else {
-    return <p> too short</p>
-  }
-}
+// function Welcome(props) {
+//   if (props.name.length > 4) {
+//     return <h1>Hello, {props.name}</h1>
+//   }
+//   else {
+//     return <p> too short</p>
+//   }
+// }
 
 
 export default function App() {
@@ -32,8 +33,11 @@ export default function App() {
             <Link to="/about">About</Link>
           </li>
           <li>
-            <Link to="/users">Users</Link>
+            <Link to="/project">Project</Link>
           </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>  
         </ul>
       </nav>
 
@@ -44,35 +48,27 @@ export default function App() {
             {/* <About /> */}
             <div className="App">
             <header className="App-header">
-              <Welcome name="Didarul"/>
+              {/* <Welcome name="Didarul"/> */}
               <img src={logo} className="App-logo" alt="logo" />
               <p>
                 About <code>src/App.js</code> and save to reload.
               </p>
-              <a
-                className="App-link"
-                href="https://reactjs.org"
-                target="_blank"
-                rel="noopener noreferrer"
+              <a className="App-link" href="https://reactjs.org"target="_blank" rel="noopener noreferrer"
               >
                 Learn React
               </a>
             </header>
             </div>
         </Route>
-          <Route path="/users">
+          <Route path="/project">
                 <div className="App">
             <header className="App-header">
-              <Welcome name="Didarul"/>
+              {/* <Welcome name="Didarul"/> */}
               <img src={logo} className="App-logo" alt="logo" />
               <p>
-                Edit <code>src/App.js</code> and save to reload.
+                Project <code>src/App.js</code> and save to reload.
               </p>
-              <a
-                className="App-link"
-                href="https://reactjs.org"
-                target="_blank"
-                rel="noopener noreferrer"
+              <a className="App-link" href="https://reactjs.org"target="_blank" rel="noopener noreferrer"
               >
                 Learn React
               </a>
@@ -84,21 +80,34 @@ export default function App() {
             {/* <Home /> */}
             <div className="App">
             <header className="App-header">
-              <Welcome name="Didarul"/>
+              {/* <Welcome name="Didarul"/> */}
               <img src={logo} className="App-logo" alt="logo" />
               <p>
                 Home <code>s rc/App.js</code> and save to reload.
               </p>
-              <a
-                className="App-link"
-                href="https://reactjs.org"
-                target="_blank"
-                rel="noopener noreferrer"
+              <a className="App-link" href="https://reactjs.org"target="_blank" rel="noopener noreferrer"
               >
                 Learn React
               </a>
             </header>
           </div>
+          </Route>
+          <Route path="/contact">
+            {/* <About /> */}
+            <div className="App">
+            <header className="App-header">
+              {/* <Welcome name="Didarul"/> */}
+              <img src={logo} className="App-logo" alt="logo" />
+              <p>
+                Contact <code>s rc/App.js</code> and save to cc.
+              </p>
+              <a
+                className="App-link" href="https://reactjs.org"target="_blank" rel="noopener noreferrer"
+              >
+                Learn React
+              </a>
+            </header>
+            </div>
         </Route>
       </Switch>
     </div>
